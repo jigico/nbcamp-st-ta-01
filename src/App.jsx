1770e9313650930349b5ac3f08d8569043dfff60
jisu <ahteostnqls@naver.com>
2024-02-06 15:28:59 +0900

    완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
+import { useState } from "react";
 import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { minusNum, plusNum } from "./redux/modules/calculator";
 
 function App() {
+  const number = useSelector((state) => state.calculator.number);
+  console.log(number);
+  const [num, setNum] = useState("");
+  const dispatch = useDispatch();
+
+  const changeHandler = (e) => {
+    console.log(e.target.value);
+    setNum(e.target.value);
+  };
+
+  const plusBtn = () => {
+    dispatch(plusNum(num));
+  };
+
+  const minusBtn = () => {
+    dispatch(minusNum(num));
+  };
+
   return (
     <div className="App">
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
+        <input type="number" value={num} onChange={(e) => changeHandler(e)} /> 만큼을 <button onClick={plusBtn}>더할게요</button> <button onClick={minusBtn}>뺄게요</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{number}</p>
       </div>
     </div>
   );
--- src/redux/modules/calculator.js
+++ src/redux/modules/calculator.js
@@ -1,10 +1,43 @@
+//action value
+const PLUS_NUM = "ADD_NUM";
+const MINUS_NUM = "MINUS_NUM";
+
+//action creator
+export const plusNum = (payload) => {
+  return {
+    type: PLUS_NUM,
+    payload
+  };
+};
+
+export const minusNum = (payload) => {
+  return {
+    type: MINUS_NUM,
+    payload
+  };
+};
+
 // 초기 상태값
 const initialState = {
-  number: 0,
+  number: 0
 };
 
 // 리듀서
-const calculator = (state = initialState, action) => {};
+const calculator = (state = initialState, action) => {
+  switch (action.type) {
+    case PLUS_NUM:
+      console.log(state.number, action.payload);
+      return {
+        number: state.number + Number(action.payload)
+      };
+    case MINUS_NUM:
+      return {
+        number: state.number - Number(action.payload)
+      };
+    default:
+      return state;
+  }
+};
 
 // 모듈파일에서는 리듀서를 export default 한다.
 export default calculator;
